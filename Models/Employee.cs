c0d3a472f683b62f8bcda85c08e39ba113fec085
RamanKarpenka <s34457@pjwstk.edu.pl>
2026-03-22 20:57:57 +0100

    add Student.cs and Employee.cs

---
+++ Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace apbd_cw3_git_s34457.Models;
+
+public class Employee : User
+{
+    public override int MaxRentals => 5;
+
+    public Employee(string f, string l) : base(f, l) { }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace apbd_cw3_git_s34457.Models;
+
+public class Student : User
+{
+    public override int MaxRentals => 2;
+
+    public Student(string f, string l) : base(f, l)  { }
+}
